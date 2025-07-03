898656e5fd39d8790fb141a8dba41a35531b06bb
Ethan Dawes <ethand.python@gmail.com>
2025-07-03 12:20:10 +0300

    Add new extension to watch

--- main/js/background.js
+++ main/js/background.js
@@ -1,9 +1,10 @@
 const enforce = new Set([
+    // pinned means the `key` manifest value is set so the id can't change.
     "behjadmmpafnpaeijfoioegdpcbpbhij", // Strict focused youtube (local & web store)
-    "bphljigopmbjfmjbdeedhmeadmefcjbf", // Watchdog (local)
-    "aeieaakfhkjhlaijcboomiokalopdlpc", // Watchdog (web store)
+    "bphljigopmbjfmjbdeedhmeadmefcjbf", // Watchdog (local, pinned, too small and doesn't change often to publish to web store)
     "kpomkjpijklmdabpfgphmapbopjockgd", // Intentional Web (web store)
-    "ejnaicioaeopejbifeboomfpcommcnng", // Intentional Web (local, not pinned so can change)
+    "ekggabbmjplmglalfjdgkogdoebccheo", // Intentional Web (local, NOT pinned so CAN change)
+    "gjmolpoegdndgefehdjnfonmahjcefhi", // Incognito Blocker (local, pinned)
 ]);
 
 chrome.management.onDisabled.addListener(info => {
